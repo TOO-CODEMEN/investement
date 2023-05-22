2ea43768b31bf11d3ff595f91c81314eeef2b951
oliceglad <vycheslavofficial@gmail.com>
2023-05-22 17:17:26 +0400

    added adpative for components: Register and Auth

--- frontend/src/Components/Pages/Registration/Register.jsx
+++ frontend/src/Components/Pages/Registration/Register.jsx
@@ -25,14 +25,15 @@ export const Register = () => {
             alert("Введите ФИО")
         } else if (validator.isInt(register.name)) {
             alert("Введите ФИО верно")
+
+        } else if (!validator.isInt(register.inn) || register.inn.length < 10) {
+            alert("Введите верно ИНН, не менее 10 символов")
         } else if (!validator.isEmail(register.email)) {
             alert("Вы не ввели E-mail")
         } else if (!validator.isStrongPassword(register.password, { minSymbols: 0 })) {
             alert("Пароль должен состоять из одной строчной, прописной буквы и цифры, не менее 8 символов")
-        } else if (!validator.isInt(register.inn) || register.inn.length < 10) {
-            alert("Введите верно ИНН, не менее 10 символов")
         } else {
-            console.log('все норм')
+            alert("Успешно!")
         }
     }
 
@@ -45,22 +46,22 @@ export const Register = () => {
                 submitHandler
             }>
                 <div className={cl.flex__container}>
-                    <div className={cl.input}><Input type="text" label="ФИО" value={register.name} setValue={setRegister} typeObject={'name'} /></div>
-                    <div className={cl.input}><Input type="text" label="Страна" value={register.country} setValue={setRegister} typeObject={'country'} /></div>
-                    <div className={cl.input}><Input type="text" label="Город" value={register.city} setValue={setRegister} typeObject={'city'} /></div>
+                    <div className={cl.input}><Input type="text" label="ФИО*" value={register.name} setValue={setRegister} typeObject={'name'} object={register} /></div>
+                    <div className={cl.input}><Input type="text" label="Страна" value={register.country} setValue={setRegister} typeObject={'country'} object={register} /></div>
+                    <div className={cl.input}><Input type="text" label="Город" value={register.city} setValue={setRegister} typeObject={'city'} object={register} /></div>
                 </div>
 
                 <div className={cl.flex__container}>
-                    <div className={cl.input}><Input type="text" label="ИНН" value={register.inn} setValue={setRegister} typeObject={'inn'} /></div>
-                    <div className={cl.input}><Input type="text" label="Название организации" value={register.nameOrganization} setValue={setRegister} typeObject={'nameOrganization'} /></div>
-                    <div className={cl.input}><Input type="text" label="Веб-сайт организации" value={register.webSite} setValue={setRegister} typeObject={'webSite'} /></div>
-                    <div className={cl.input}><Input type="text" label="Отрасль ведения хоз.деятельности" value={register.industry} setValue={setRegister} typeObject={'industry'} /></div>
-                    <div className={cl.input}><Input type="text" label="Должность" value={register.job} setValue={setRegister} typeObject={'job'} /></div>
+                    <div className={cl.input}><Input type="text" label="ИНН*" value={register.inn} setValue={setRegister} typeObject={'inn'} object={register} /></div>
+                    <div className={cl.input}><Input type="text" label="Название организации" value={register.nameOrganization} setValue={setRegister} typeObject={'nameOrganization'} object={register} /></div>
+                    <div className={cl.input}><Input type="text" label="Веб-сайт организации" value={register.webSite} setValue={setRegister} typeObject={'webSite'} object={register} /></div>
+                    <div className={cl.input}><Input type="text" label="Отрасль ведения хоз.деятельности" value={register.industry} setValue={setRegister} typeObject={'industry'} object={register} /></div>
+                    <div className={cl.input}><Input type="text" label="Должность" value={register.job} setValue={setRegister} typeObject={'job'} object={register} /></div>
                 </div>
 
                 <div className={cl.flex__container}>
-                    <div className={cl.input}><Input type="email" label="E-mail" value={register.email} setValue={setRegister} typeObject={'email'} /></div>
-                    <div className={cl.input}><Input type="password" label="пароль" value={register.password} setValue={setRegister} typeObject={'password'} /></div>
+                    <div className={cl.input}><Input type="email" label="E-mail*" value={register.email} setValue={setRegister} typeObject={'email'} object={register} /></div>
+                    <div className={cl.input}><Input type="password" label="пароль*" value={register.password} setValue={setRegister} typeObject={'password'} object={register} /></div>
                 </div>
                 <button className={cl.form__button}>
                     Зарегистрироваться
