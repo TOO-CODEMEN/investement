abe5e652aaff08aa97d7ca8dac5fb201d5d8c1f9
oliceglad <vycheslavofficial@gmail.com>
2023-05-25 22:10:24 +0400

    fix bugs with checkbox

--- frontend/src/Components/Pages/Main/Main.jsx
+++ frontend/src/Components/Pages/Main/Main.jsx
@@ -21,6 +21,8 @@ const Main = () => {
         hardware: "",
         objectType: "",
         objectArea: "",
+        accounting: true,
+        patent: false
     })
 
 
@@ -40,17 +42,6 @@ const Main = () => {
         { value: 'Офисное здание', label: 'Офисное здание' },
         { value: 'Складское помещение', label: 'Складское помещение' },
     ]
-
-    const [patent, setPatent] = useState(false)
-    const [accounting, setAccounting] = useState(false)
-
-    const patentChange = () => {
-        setPatent(!patent);
-    };
-
-    const accountingChange = () => {
-        setAccounting(!accounting);
-    };
 
     // useEffect(() => {
     //     let session = getSession();
@@ -193,8 +184,8 @@ const Main = () => {
                     <div className={cl.input}><Input type="text" label="Площадь объекта" value={calc.objectArea} setValue={setCalc} object={calc} typeObject={'objectArea'} /></div>
                 </div>
 
-                <Checkbox id="accounting" label="Предоставление бухгалтерских услуг" name="accounting" value="accounting" checked={accounting} onChange={accountingChange} />
-                <Checkbox id="patent" label="Оформление патента (только для ИП)" name="patent" value="patent" checked={patent} onChange={patentChange} />
+                <Checkbox id="accounting" label="Предоставление бухгалтерских услуг" name="accounting" checked={calc.accounting} setChange={setCalc} typeObject = {'accounting'}/>
+                <Checkbox id="patent" label="Оформление патента (только для ИП)" name="patent"  checked={calc.patent} setChange={setCalc} typeObject={'patent'}/>
 
                 <button className={cl.form__button}>
                     Рассчитать
--- frontend/src/Components/UI/Checkbox/Checkbox.jsx
+++ frontend/src/Components/UI/Checkbox/Checkbox.jsx
@@ -4,7 +4,10 @@ import cl from './Checkbox.module.css'
 const Checkbox = (props) => {
     return (
         <div>
-            <input type="checkbox" className={cl.checkbox} id={props.id} name={props.name} value={props.value} checked={props.checked} onChange={props.onChange}></input>
+            <input type="checkbox" className={cl.checkbox} id={props.id} name={props.name} checked={props.checked} onChange={() => props.setChange((value) => ({
+                ...value, 
+                [props.typeObject]: !props.checked
+            }))} />
             <label htmlFor={props.id} >{props.label}</label>
         </div>
     )
